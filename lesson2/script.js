ad7b9116e37f4124c1b376263b78edd43f542fed
MherKaroyan521 <mher.karoyan@tumo.org>
2023-02-12 12:43:54 +0400

    lesson2

---
+++ lesson2/script.js
@@ -0,0 +1,20 @@
+class Node
+{
+    constructor(data,next)
+    {
+        this.data = data
+        this.next = next
+    }
+}
+m=0
+let list={
+    "0":new Node(null,null)
+}
+console.log(list)
+ if (list.m.data == null)
+ {
+     list.m("gsttb",null)
+ }
+ else {
+     list.push(new Node("gsttb",null))
+ }
